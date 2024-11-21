a5bfcbd50bb96ca4e4e1d5a223cdec130e59025f
NaturalSelect <2145973003@qq.com>
2024-11-21 16:52:18 +0800

    feat(weak2): finish jobs
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -1,36 +1,81 @@
 use grid::Grid; // For lcs()
 use std::env;
-use std::fs::File; // For read_file_lines()
+use std::fs::{read, File}; // For read_file_lines()
 use std::io::{self, BufRead}; // For read_file_lines()
 use std::process;
 
 pub mod grid;
 
-/// Reads the file at the supplied path, and returns a vector of strings.
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let fp = File::open(filename);
+    if let Err(err) = fp {
+        return Err(err);
+    }
+    let fp = fp.unwrap();
+    let mut lines: Vec<String> = Vec::new();
+    for i in io::BufReader::new(&fp).lines() {
+        if let Err(err) = i {
+            return Err(err);
+        }
+        lines.push(i.unwrap());
+    }
+    return Ok(lines);
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+
+    let seq1_len = seq1.len();
+    let seq2_len = seq2.len();
+
+    let mut c = Grid::new(seq1_len + 1, seq2_len + 1);
+    for i in 0..=seq1_len {
+        c.set(i, 0, 0).unwrap();
+    }
+    for i in 0..=seq2_len {
+        c.set(0, i, 0).unwrap();
+    }
+
+    for i in 0..seq1_len {
+        for j in 0..seq2_len {
+            if seq1[i] == seq2[j] {
+                c.set(i + 1, j + 1, c.get(i, j).unwrap() + 1).unwrap();
+            } else {
+                let max = if c.get(i + 1, j).unwrap() > c.get(i, j + 1).unwrap() {
+                    c.get(i + 1, j).unwrap()
+                } else {
+                    c.get(i, j + 1).unwrap()
+                };
+                c.set(i + 1, j + 1, max).unwrap();
+            }
+        }
+    }
+    return c;
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn print_diff(lcs_table: &Grid, lines1: &Vec<String>, lines2: &Vec<String>, i: usize, j: usize) {
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    if i > 0 && j > 0 && lines1[i - 1] == lines2[j - 1] {
+        print_diff(lcs_table, lines1, lines2, i - 1, j - 1);
+        print!("  {}\n", lines1[i - 1]);
+    } else if j > 0
+        && (i == 0 || lcs_table.get(i, j - 1).unwrap() >= lcs_table.get(i - 1, j).unwrap())
+    {
+        print_diff(lcs_table, lines1, lines2, i, j - 1);
+        print!(">  {}\n", lines2[j - 1]);
+    } else if i > 0
+        && (j == 0 || lcs_table.get(i, j - 1).unwrap() < lcs_table.get(i - 1, j).unwrap())
+    {
+        print_diff(lcs_table, lines1, lines2, i - 1, j);
+        print!("<  {}\n", lines1[i - 1]);
+    } else {
+        print!("");
+    }
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -40,8 +85,24 @@ fn main() {
     let filename1 = &args[1];
     let filename2 = &args[2];
 
-    unimplemented!();
-    // Be sure to delete the #[allow(unused)] line above
+    let lines1 = read_file_lines(filename1);
+    let lines2 = read_file_lines(filename2);
+
+    if let Err(err) = lines1.as_ref() {
+        println!("Error reading file {}: {}", filename1, err);
+        process::exit(1);
+    }
+
+    if let Err(err) = lines2.as_ref() {
+        println!("Error reading file {}: {}", filename2, err);
+        process::exit(1);
+    }
+
+    let lines1 = lines1.unwrap();
+    let lines2 = lines2.unwrap();
+
+    let lcs_table = lcs(&lines1, &lines2);
+    print_diff(&lcs_table, &lines1, &lines2, lines1.len(), lines2.len());
 }
 
 #[cfg(test)]
--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::BufRead;
 use std::process;
 
 fn main() {
@@ -9,4 +11,26 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    let fp = File::open(filename);
+    if let Err(err) = fp {
+        println!("Error: {}", err);
+        process::exit(1);
+    }
+    let fp = fp.unwrap();
+    let mut line_Cnt = 0;
+    let mut word_Cnt = 0;
+    let mut char_Cnt = 0;
+
+    for line in std::io::BufReader::new(fp).lines() {
+        if let Err(err) = line {
+            println!("Error: {}", err);
+            process::exit(1);
+        }
+        let line = line.unwrap();
+        line_Cnt += 1;
+        char_Cnt += line.len();
+        word_Cnt += line.split_whitespace().count();
+    }
+
+    println!("{} {} {}", line_Cnt, word_Cnt, char_Cnt);
 }
